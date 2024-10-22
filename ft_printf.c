152c7f31523d030757b2132f2b6e73aef1efc146
Julio Max Calderone Da Silva <julcalde@2-E-7.42heilbronn.de>
2024-10-22 17:12:36 +0200

    started working on 'ft_printf.c'. To be continued...

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 16:13:47 by julcalde          #+#    #+#             */
-/*   Updated: 2024/10/22 16:35:56 by julcalde         ###   ########.fr       */
+/*   Updated: 2024/10/22 17:10:25 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,16 @@
 
 int	ft_printf(const char *format, ...)
 {
+	int	i;
+
+	i = 0;
+	if (format != NULL)
+	{
+		while (format[i] != '\0')
+		if(format[i] == '%' && format[i + 1] != '%')
+		{
+			//check if next index asks for format specifier
+		}
+	}
 	return (0);
 }
